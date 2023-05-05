ecd6da79f158362615e9384b0d695d27f20ea96f
nuriaramoos <132547909+nuriaramoos@users.noreply.github.com>
2023-05-05 12:27:36 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+basic.showString("\"hola\"")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+}
+basic.clearScreen()
+basic.showString("\"adios\"")
+basic.forever(function () {
+	
+})
